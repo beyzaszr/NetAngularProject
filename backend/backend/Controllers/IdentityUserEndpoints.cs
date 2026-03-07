8b37f18030bcddc922e85240e0126f50c9683b05
Beyza Sezer <byzaszr@outlook.com>
2026-03-07 20:16:06 +0300

    updated model layer

--- backend/backend/Controllers/IdentityUserEndpoints.cs
+++ backend/backend/Controllers/IdentityUserEndpoints.cs
@@ -9,25 +9,6 @@ using System.Text;
 
 namespace backend.Controllers
 {
-
-    public class UserRegistrationModel
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string PhoneNumber { get; set; }
-        // Angular tarafındaki file input'tan gelen dosyayı bu karşılar
-        public IFormFile? ProfileImageUrl { get; set; }
-    }
-
-    public class UserLoginModel
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-
-
     public static class IdentityUserEndpoints
     {
 
---
+++ backend/backend/Models/UserRegistrationModel.cs
@@ -0,0 +1,13 @@
+﻿namespace backend.Models
+{
+    public class UserRegistrationModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        // Angular tarafındaki file input'tan gelen dosyayı bu karşılar
+        public IFormFile? ProfileImageUrl { get; set; }
+    }
+}
